321b7376a74754f686c5c44051de55da85aa1232
maisjamil1 <maisjamil17118@gmail.com>
2020-04-05 19:59:20 +0300

    3

--- server.js
+++ server.js
@@ -19,13 +19,42 @@ app.get('/location', (request, response) => {
   }
 });
 
-app.use('*', notFoundHandler);
+
 function Location(city, geoData) {
   this.search_query = city;
   this.formatted_query = geoData[0].display_name;
   this.latitude = geoData[0].lat;
   this.longitude = geoData[0].lon;
 }
+
+//____________________
+app.get('/weather', (request, response) => {
+  try {
+    const weatherData = require('./data/darksky.json');
+    const city = request.query.city;
+    const locationData = new Location(city, geoData);
+    response.status(200).json(locationData);
+  } catch (error) {
+    errorHandler(error, request, response);
+  }
+});
+
+
+function weather(city, geoData) {
+  this.search_query = city;
+  this.formatted_query = geoData[0].display_name;
+  this.latitude = geoData[0].lat;
+  this.longitude = geoData[0].lon;
+}
+
+
+
+
+
+
+
+//______________________________________________
+app.use('*', notFoundHandler);
 
 function notFoundHandler(request, response) {
   response.status(404).send('NOT FOUND!!');
